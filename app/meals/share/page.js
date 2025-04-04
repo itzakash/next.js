ad890d64990af3b0b525105acca78437aa79c795
Akash Talawar <akasht@birlasoft.com>
2025-04-04 14:07:54 +0530

    added the revalidatePath feature

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,10 +1,12 @@
+'use client'
 import MealImageUpload from '../../../components/meals/image-picker';
-import MealsFormSubmit from '../../../components/meals/meals-form-submit';
 import { shareMeal } from '../../../lib/action';
 import classes from './page.module.css';
+import { useActionState } from 'react';
 
 export default function ShareMealPage() {
 
+    const [state, formAction,isPending] = useActionState(shareMeal,{message : null})
  
     return (
         <>
@@ -15,7 +17,7 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form} action={shareMeal}>
+                <form className={classes.form} action={formAction}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
@@ -44,9 +46,9 @@ export default function ShareMealPage() {
                         ></textarea>
                     </p>
                     <MealImageUpload label="Upload your meal image's" name="image"  />
+                    {state.message && <p>{state.message}</p>}
                     <p className={classes.actions}>
-                        {/* <button type="submit">Share Meal</button> */}
-                        <MealsFormSubmit />
+                        <button type="submit">{isPending ? 'Submitting' : 'Share Meal'}</button>
                     </p>
                 </form>
             </main>
--- components/meals/meals-form-submit.js
+++
@@ -1,8 +0,0 @@
-'use client'
-import { useFormStatus } from 'react-dom'
-
-export default function MealsFormSubmit() {
-  const { pending } = useFormStatus()
-
-  return <button type="submit">{pending ? 'Submitting' : 'Share Meal'}</button>
-}
